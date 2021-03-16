62638fb9ddb1fe9579fe4b8b802633a852f2108d
mrmeysam2 <mr.meysam2@gmail.com>
2021-03-16 14:20:04 +0330

    test js

--- src/app/features/layouts/admin-layout/admin-layout.component.ts
+++ src/app/features/layouts/admin-layout/admin-layout.component.ts
@@ -7,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminLayoutComponent implements OnInit {
 
+  res: any;
+
+  map: any;
+  slice: any[];
+  join: any;
+  indexOf: number;
+  lastIndexOf: number;
+  sort: any[];
+
+
   constructor() { }
 
 
   ngOnInit(): void {
-  }
+    let obj: Array<any> = ['1', '2', '3', '4', '5'];
+    let objSort: Array<any> = ['2', '1', '6', '4', '9'];
+    let objNumber: Array<any> = [1, 2, 3, 4, 5];
 
+    // Slider 1
+    this.map = objNumber.map(x => x * 2)      // 2,4,6,8,10
+    this.slice = obj.slice(2)                 // 3,4,5
+    this.join = obj.join('---')               // 1---2---3---4---5
+    this.indexOf = obj.indexOf('3')           // 2
+    this.lastIndexOf = obj.lastIndexOf('3')   // 2
+
+    // slider 2
+    this.sort = objSort.sort()   // 2
+  }
 }
